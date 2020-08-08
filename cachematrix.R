61895b779ed5ba7cb2a5eb5fbd6554addd19ec8d
Ipshita Ghosh <iuippi3@gmail.com>
2020-08-08 18:30:05 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A set of functions to compute and cache the inverse of a square matrix
 
-## Write a short comment describing this function
-
+## A function to create a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+	##initialize i as NULL
+	i<-NULL
+	##create set function to set values of x and i
+	set <- function(y) {
+		x<<-y
+		i<<-NULL
+		}
+	##create get function which returns x matrix	
+	get <- function() x
+	##create setinv value to cache inverse of matrix
+	setinv <-function(solve) i <<- solve
+	##create getinv function to return cached inverse
+	getinv <- function() i
+	##create list of functions
+	list(set = set, get = get, 
+		setinv = setinv, 
+		getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## A function that takes the special "matrix" created with makeCacheMatrix as argument
+## if inverse has been calculated, returns cached inverse
+## if not, it calculates the inverse and saves to cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        ## Return a matrix that is the inverse of special matrix 'x'
+		## this matrix must be created with the makeCacheMatrix function
+		
+		##check value of i to see if inverse is cached
+		i<- x$getinv()
+		##if inverse matrix is cached i is not NULL
+		if(!is.null(i)){
+			##return message that data is cached
+			message("Getting cached data")
+			##return cached inverse
+			return(i)
+		}
+		##if inverse matrix is NOT cached, i IS NULL
+		##load values of matrix x as variable matrix1
+		matrix1 <- x$get()
+		##make value of i equal to inverse of matrix1 with solve function
+		i <- solve(matrix1)
+		##set inverse value i for special matrix 'x'
+		x$setinv(i)
+		##return inverse of the matrix
+		i	
 }
